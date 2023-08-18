769cb8eb3138673edad46ed90d56fba48a9df4f9
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-08-18 21:25:38 +0300

    Step 1: update find debris

--- includes/pages/game/ShowFindDebrisPage.class.php
+++ includes/pages/game/ShowFindDebrisPage.class.php
@@ -36,19 +36,40 @@ class ShowFindDebrisPage extends AbstractGamePage
             $range = 0;
         }
         
+        $table = "<tbody class='row' style='padding: 7px;'>";
+        
         if($mode == '1'){
             $cautare = $GLOBALS['DATABASE']->query("SELECT *from ".PLANETS." where (`der_metal` >0 OR `der_crystal` >0) AND (`system` > '".($PLANET['system'] - $range)."' AND `system` < '".($PLANET['system'] + $range)."') AND `galaxy` = '".$PLANET['galaxy']."' and `planet_type` = '1' ;");
-			$table = "<tr><th>".$LNG['debris_action_1']."</th><th>".$LNG['debris_action_2']."</th><th>".$LNG['debris_action_3']."</th><th>".$LNG['debris_action_4']."</th><th>".$LNG['debris_action_5']."</th><th>".$LNG['debris_action_6']."
-			</th></tr>";
+			$table .= "
+                <tr class='row col-12 barraclass'>
+                    <th class='col-1'>".$LNG['debris_action_1']."</th>
+                    <th class='col-1'>".$LNG['debris_action_2']."</th>
+                    <th class='col-1'>".$LNG['debris_action_3']."</th>
+                    <th class='col-4'>".$LNG['debris_action_4']."</th>
+                    <th class='col-4'>".$LNG['debris_action_5']."</th>
+                    <th class='col-1'>".$LNG['debris_action_6']."</th>
+                </tr>
+            ";
 			
             if($GLOBALS['DATABASE']->numRows($cautare) > 0)
             while($GalaxyRowPlanet = $GLOBALS['DATABASE']->fetch_array($cautare)){
                 $GRecNeeded = min(ceil(($GalaxyRowPlanet['der_metal'] + $GalaxyRowPlanet['der_crystal']) / $pricelist[219]['capacity']), $PLANET[$resource[219]]);
-                $table .= "<tr><td>".$GalaxyRowPlanet['galaxy']."</td><td>".$GalaxyRowPlanet['system']."</td><td>".$GalaxyRowPlanet['planet']."</td><td>".pretty_number($GalaxyRowPlanet['der_metal'])."</td><td>".pretty_number($GalaxyRowPlanet['der_crystal'])."</td><td><a href='javascript:doit(8,".$GalaxyRowPlanet['id'].");'>".$LNG['debris_action_6']."</a></td></tr>";
+                $table .= "
+                    <tr class='row col-12 mt-1 classificabarra'>
+                        <td class='col-1'>".$GalaxyRowPlanet['galaxy']."</td>
+                        <td class='col-1'>".$GalaxyRowPlanet['system']."</td>
+                        <td class='col-1'>".$GalaxyRowPlanet['planet']."</td>
+                        <td class='col-4'>".pretty_number($GalaxyRowPlanet['der_metal'])."</td>
+                        <td class='col-4'>".pretty_number($GalaxyRowPlanet['der_crystal'])."</td>
+                        <td class='col-1'><a href='javascript:doit(8,".$GalaxyRowPlanet['id'].");'>".$LNG['debris_action_6']."</a></td>
+                    </tr>
+                ";
             }else{
                 $table .= "<tr><td colspan='6'>".$LNG['debris_action_7']."</td></tr>";
             }
 		}
+        
+        $table .= "</tbody>";
         
         $this->tplObj->assign_vars(array(
             'range' => $range,
--- styles/theme/nsu/templates/game/page.findDebris.default.tpl
+++ styles/theme/nsu/templates/game/page.findDebris.default.tpl
@@ -1,28 +1,37 @@
 {block name="title" prepend}{$LNG.lm_finddebris}{/block}
 {block name="content"}
+<link rel="stylesheet" type="text/css" href="{$dpath}css/statistics.css">
 <div id="page">
     <div id="content">
         <div id="ally_content" class="conteiner">
             <div class="gray_stripo">{$LNG.lm_finddebris}
                 <a class="tornaindietro" href="?page=findDebris&y=1">{$LNG.debris_show} ({$LNG.debris_range}: {$range})</a>
             </div>
-            <div class="ally_img">
-                <table class="no_visible">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <img src="{$dpath}img/title/findDebris.jpg" alt="*">
-                                <span class="designation">
-                                    <span>{$LNG.lm_finddebris}</span><br>
-                                </span>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div class="row" style="padding: 7px">
+                <div class="col-md-12">
+                    <div class="card valueTest2"> 
+                        <div class="ally_img">
+                            <table class="no_visible">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <img src="{$dpath}img/title/findDebris.jpg" alt="*">
+                                            <span class="designation">
+                                                <span>{$LNG.lm_finddebris}</span>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>            
+                    </div>
+                </div>
             </div>
-   <table class="tablesorter ally_ranks">
-      {$debris}
-   </table>
+            <table class="tablesorter ally_ranks tabstatistica">
+                {$debris}
+            </table>
+        </div>
+    </div>
 </div>
 <script>
    function doit(missionID, planetID) {
